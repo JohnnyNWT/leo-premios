e92fca7ba51ce381f8f999a9bb5f3ecdd23c5c57
JohnnyNWT <jooaaooskt@gmail.com>
2024-07-22 12:36:56 -0300

    FEAT: add data.js

---
+++ src/app/data/data.js
@@ -0,0 +1,14 @@
+const ACOES = {
+  "rifa-bombox": {
+    titulo: "BOMBOX OU R$2.000,00 NA CONTA",
+    desc: "Bombox nova na caixa ou R$2.000,00 na conta",
+    sorteio: "13/07/24 às 19h00",
+    valor: "R$ 0,01",
+    status: "Concluído",
+    ganhador: "Renato Silva",
+    ganhadorDesc: "Ganhador(a) com o número da sorte",
+    cota: "147527",
+  },
+}
+
+export default ACOES;
